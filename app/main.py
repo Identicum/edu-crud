3db5d87e6cdc46700d29cada9aa10da943a5cebc
Gustavo J Gallardo <ggallard@identicum.com>
2026-03-01 01:39:01 -0300

    minor updates

--- app/main.py
+++ app/main.py
@@ -1,46 +1,44 @@
-from flask import Flask, request, jsonify
-from flask import render_template, redirect, url_for, flash # Import flash
-from models import db, Person, Course, Teacher, Student #
-import config as config #
-import os #
-from sherpa.utils.basics import Logger #
+from flask import Flask, render_template
+from models import db, Person, Course, Teacher, Student
+import config as config
+import os
+from sherpa.utils.basics import Logger
 
-# Import Blueprints
-from blueprints.persons import persons_bp #
-from blueprints.courses import courses_bp #
-from blueprints.teachers import teachers_bp #
-from blueprints.students import students_bp #
+from blueprints.persons import persons_bp
+from blueprints.courses import courses_bp
+from blueprints.teachers import teachers_bp
+from blueprints.students import students_bp
 
-app = Flask(__name__) #
-log_level = os.environ.get("LOG_LEVEL", "") #
-logger = Logger("python-flask", log_level=log_level, log_path="/tmp/python-flask.log") #
-logger.info("Logger initialized with level: {}.", log_level) #
-app.config.from_object(config) #
-db.init_app(app) #
+app = Flask(__name__)
+log_level = os.environ.get("LOG_LEVEL", "")
+logger = Logger("edu-crud", log_level=log_level, log_path="/tmp/edu-crud.log")
+logger.info("Logger initialized with level: {}.", log_level)
+app.config.from_object(config)
+db.init_app(app)
 
-@app.route('/health', methods=["GET"]) #
+@app.route('/health', methods=["GET"])
 def getHealth():
-    logger.trace("Health endpoint called.") #
-    return 'OK' #
+    logger.trace("Health endpoint called.")
+    return 'OK'
 
-@app.route("/") #
+@app.route("/")
 def home():
-    return render_template("index.html") #
+    return render_template("index.html")
 
 # Register Blueprints
-app.register_blueprint(persons_bp) #
-app.register_blueprint(courses_bp) #
-app.register_blueprint(teachers_bp) #
-app.register_blueprint(students_bp) #
+app.register_blueprint(persons_bp)
+app.register_blueprint(courses_bp)
+app.register_blueprint(teachers_bp)
+app.register_blueprint(students_bp)
 
 # ---------- Helper ----------
-def model_as_dict(self): #
-    return {c.name: getattr(self, c.name) for c in self.__table__.columns} #
+def model_as_dict(self):
+    return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
-Person.as_dict = model_as_dict #
-Course.as_dict = model_as_dict #
-Teacher.as_dict = model_as_dict #
-Student.as_dict = model_as_dict #
+Person.as_dict = model_as_dict
+Course.as_dict = model_as_dict
+Teacher.as_dict = model_as_dict
+Student.as_dict = model_as_dict
 
-if __name__ == "__main__": #
-    app.run(debug=True, host="0.0.0.0") #
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
